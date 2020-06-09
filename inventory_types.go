f58f7fafd3dd4ee121821cc81ba62f368a73f07e
Tommy McNeely <thomas.mcneely@davita.com>
2020-06-09 16:23:25 -0600

    Attempt 2: deal with structured facts

--- inventory_types.go
+++ inventory_types.go
@@ -9,9 +9,18 @@ A representation of an inventory query response.
 More details here: https://puppet.com/docs/puppetdb/5.2/api/query/v4/inventory.html#response-format
 */
 type Inventory struct {
-	Certname    string            `json:"certname"`
-	Timestamp   string            `json:"timestamp"`
-	Environment string            `json:"environment"`
-	Facts       map[string]string `json:"facts"`
-	Trusted     map[string]string `json:"trusted"`
+	Certname    string                 `json:"certname"`
+	Timestamp   string                 `json:"timestamp"`
+	Environment string                 `json:"environment"`
+	Facts       map[string]interface{} `json:"facts"`
+	Trusted     map[string]interface{} `json:"trusted"`
+}
+
+/*
+InventoryFact struct
+Facts contained in the inventory
+*/
+type InventoryFact struct {
+	Name  string                   `json:"name"`
+	Value map[string]InventoryFact `json:"value"`
 }
